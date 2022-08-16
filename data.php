003891edecd9f27b8c19944a9d77eef91bc9da1f
suparat23 <110450568+suparat23@users.noreply.github.com>
2022-08-16 14:30:05 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,14 @@
+<?php
+$title=$_POST['title'];
+
+include 'database.php';
+$sql="INSERT INTO todoss(title)VALUES('$title')";
+
+$result=mysqli_query($conn, $sql);
+
+if($result){
+    header("location: ./index.php");
+}
+else{
+
+}
